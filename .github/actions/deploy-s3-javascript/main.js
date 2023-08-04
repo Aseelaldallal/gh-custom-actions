78073a884826e06eb0b90a636bfbbd973edc5cff
Aseel Al Dallal <aseelaldallal@gmail.com>
2023-08-03 20:49:19 -0700

    chore: test

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,7 +4,7 @@ import exec from '@actions/core';
 
 function run() { // you see this in a lot of actions, but not required
     // Log msg to github actions workflow log
-    notice('Hello from my custom JS action!');
+    
 }
 
 run();
