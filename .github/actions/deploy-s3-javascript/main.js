82c007c7dc1dabf9365e359e316b675b9bcb2bff
Aseel Al Dallal <aseelaldallal@gmail.com>
2023-08-03 21:00:36 -0700

    commonjs

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,10 +1,11 @@
-import { notice } from '@actions/core'; // utility methods and features
-import github from '@actions/github';
-import exec from '@actions/core';
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
 
 function run() { // you see this in a lot of actions, but not required
     // Log msg to github actions workflow log
-    notice('Hello from my custom JS action!');
+    core.notice('Hello from my custom JS action!');
 }
 
 run();
