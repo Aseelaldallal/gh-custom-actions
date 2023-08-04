7baf7026f2d9cae2c9b99917a459b1368932d4df
Aseel Al Dallal <aseelaldallal@gmail.com>
2023-08-03 21:30:15 -0700

    aws

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,8 +4,21 @@ const exec = require('@actions/exec');
 
 
 function run() { // you see this in a lot of actions, but not required
-    // Log msg to github actions workflow log
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', { required: true }); 
+    // required true means you have to return a value. I.e, if in the yaml file
+    // you set required to false, and there was no default value for bucketRegion,
+    // this call will throw
+    const bucketRegion = core.getInput('bucket-region', { required: true});
+    const distFolder = core.getInput('dist-folder', { required: true });
     core.notice('Hello from my custom JS action!');
+
+    // 2) Upload Files
+    const s3uri = `s3://${bucket}`
+    // aws cli installed in ubunto-latest (see github actions documentation)
+    exec.exec(`aws s3 sync ${distFolder} ${s3uri} -- region ${bucketRegion}`);
+    // exec.exec is used to execute shell commands from within a actions workflow
+    // here we are using exeucting the AWS cliecommand for synchronizing files with s3 bucket
 }
 
 run();
